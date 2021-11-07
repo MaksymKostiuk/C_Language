6803e83ba5223c84329d7f31a806bf950db25061
Max <ISomeTimesCompany@gmail.com>
2021-11-07 18:59:53 +0300

    Just have started creating new game Jumper!

---
+++ Jumper/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define mapWidth 80
+#define mapHeight 25
+
+typedef struct SObject {
+    float x, y;
+} TObject;
+
+char map[mapHeight][mapWidth+1];
+TObject jumper;
+
+void ClearMap()
+{
+    for (int i = 0; i < mapWidth; i++)
+    {
+        map[0][i] = '.';
+    }
+    map[0][mapWidth] = '\0';
+    for (int j = 0; j < mapHeight; j++)
+    {
+        sprintf( map[j], map[0]);
+    }
+}
+
+void ShowMap()
+{
+    map[mapHeight - 1][mapWidth - 1] = '\0';
+    for (int j = 0; j < mapHeight; j++)
+    {
+        printf("%s", map[j]);
+    }
+}
+
+void SetObjectPos(TObject *obj, float xPos, float yPos)
+{
+    (*obj).x = xPos;
+    (*obj).y = yPos;
+}
+
+void PutObjectOnMap(TObject obj)
+{
+    int ix = (int) round(obj.x);
+    int iy = (int) round(obj.y);
+    map[iy][ix] = '@';
+}
+
+int main()
+{
+    SetObjectPos(&jumper, 20, 10);
+    ClearMap();
+    PutObjectOnMap(jumper);
+    ShowMap();
+    return 0;
+}
